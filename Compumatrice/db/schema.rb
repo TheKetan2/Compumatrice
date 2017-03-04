a30eb994d7387507cd66cfc1f849eb7e449b0aee
TheKetan2 <ketantoo@gmail.com>
2017-03-05 03:20:02 +0530

    Heroku destroy bug resolved. rearranging js worked

--- Compumatrice/db/schema.rb
+++ Compumatrice/db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170304120617) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password"
-    t.string   "password_confirmation"
+    t.string   "confirm_password"
     t.string   "f_name"
     t.string   "lname"
     t.string   "email"
