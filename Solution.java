c70a2647abcc80bfdb2857b70dcd7a6b468e7556
TheKetan2 <ketantoo@gmail.com>
2017-03-04 17:23:01 +0530

    Question 2 Solution

---
+++ Solution.java
@@ -0,0 +1,27 @@
+/*
+Name: Ketan D Ramteke
+Question No. 2
+*/
+import java.util.*;
+
+class Solution {
+
+//Function to print out biggest sentense
+public static int bigSentence(String str){
+		String[] parts = str.split("\\.|\\?|\\!");
+		int len=0;
+		for(String a:parts){
+			//System.out.println(a.trim());
+			String[] s = a.trim().split(" ");
+			if(s.length>len)
+				len = s.length;
+	}
+return len;
+	
+}
+public static void main(String args[]){
+	
+System.out.println(bigSentence("We test coders.You all are doing great job! Give us a try?"));
+
+	}
+}
